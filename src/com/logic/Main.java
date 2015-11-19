fbdf3c3091d94d3a9b69e37d778d6ab2928e76b0
Jay <jannik9797@web.de>
2015-11-19 18:01:25 +0100

    Hab die Scheiße ein bisschen angepasst:)

--- src/com/logic/Main.java
+++ src/com/logic/Main.java
@@ -3,7 +3,11 @@ package com.logic;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Das ist Pokemon");
+		System.out.println("Das ist nicht Pokemon! Es ist Sparta!");
+		
+		for(int i = 0; i < 40; i++) {
+			System.out.println("Immer " + i + "-mal mehr wie du");
+		}
 	}
 
 }
